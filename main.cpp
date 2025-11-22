14351468cc8c6388b6c9aec0f7a4cb23ef939b92
MrMuhammadSamadSiddiqui <abdulsimplesamad318@gmail.com>
2025-11-22 21:29:45 +0500

    Implement Node structure and Add_My_Classes class
    Added Node structure and Add_My_Classes class for managing course data.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
 using namespace std;
+struct Node {
+    Node* next   ; 
+    string course_name  ; 
+     string section  ; 
+     Node(){ next  = nullptr ; }
+     Node(string c , string s ){
+          course_name = c ; 
+          section = s ; 
+          next  = nullptr ; 
+     }
+};
+
+class Add_My_Classes{
+     public : 
+     Node* head  ; 
+     Insert_Classes(string cn , string sc){
+          Node* classes = new Node(cn , sc) ; 
+          
+     }
+};
 class Rooms{
-     Search_For_Free_Rooms(string day){
+     Search_For_Free_Rooms(string day , ){
        
      }    
 };

8c42c3cef0e2a129b9d6e55a51afc67a7ce1aa4d
MrMuhammadSamadSiddiqui <abdulsimplesamad318@gmail.com>
2025-11-22 21:21:51 +0500

    Add classes for Rooms, Teachers, and Timetable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 using namespace std;
+class Rooms{
+     Search_For_Free_Rooms(string day){
+       
+     }    
+};
+class Teachers{
+     Search_Teacher(string name){
+       
+     }
+     Search_Teacher_Timetable(string name){
+       
+     }
+
+      
+};
+class Timetable {
+    Search_Timetable(string day){
+
+    }
+
+    Search_Timetable(){
+      
+    }
+    Sort_Timetable(string day){
+      
+    }
+    
+
+
+}; 
 
 int main(){
-  cout<<"LET THERE BE CODE ! ⚡"<<endl;
+  cout<<"Welcome to FAST NATIONAL UNIVERISTY KARACHI CAMPUS timetable management system" << endl ; 
 }
